65a28f1c955ff1235bf53958a6b357c2ace1a1fc
Sean Aromin <thedirtygamer208@gmail.com>
2025-02-03 01:01:57 +0800

    fixes and added timeline

--- gallery.js
+++ gallery.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $(".post-img").click(function () {
         let imgSrc = $(this).attr("src");
         $("#modalImage").attr("src", imgSrc);
-        $("#imageModal").fadeIn();
+        $("#imageModal").addClass("show"); // Adds the 'show' class to display modal
     });
 
     $("#closeModal, #imageModal").click(function (e) {
         if (e.target.id === "closeModal" || e.target.id === "imageModal") {
-            $("#imageModal").fadeOut();
+            $("#imageModal").removeClass("show"); // Removes the 'show' class to hide modal
         }
     });
 });
---
+++ timeline.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    function revealMemories() {
+        $(".memory").each(function () {
+            let position = $(this).offset().top;
+            let scrollPosition = $(window).scrollTop() + $(window).height() - 100;
+
+            if (scrollPosition > position) {
+                $(this).addClass("show");
+            }
+        });
+    }
+
+    $(window).on("scroll", revealMemories);
+    revealMemories();
+});
